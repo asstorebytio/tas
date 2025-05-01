9fdc6df85339d6ddb5839ce755a646b80ceedd0a
zpyoo <150746635+asstorebytio@users.noreply.github.com>
2025-05-01 14:20:48 +0800

    Update usernamelist.lua

--- usernamelist.lua
+++ usernamelist.lua
@@ -2,5 +2,5 @@ buyerUsername = {
 	buyerAutoRest = ["zipo","garoxx","keano1","ifhul1"],
 	buyerAutoFind = ["zipo","garoxx","icebear57"],
 	buyerAutoPNB = ["zipo","garoxx"],
-	buyerAutoTakeProfit = ["zipo","garoxx"]
+	buyerAutoTakeProfit = ["zipo","garoxx"],
 }
